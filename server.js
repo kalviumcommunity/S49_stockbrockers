b279b5c03ae323d171922ea0cd84e8bbfd40eccd
pavanKalvium1 <pavanew11@gmail.com>
2024-02-21 12:27:38 +0530

    updated

--- server.js
+++ server.js
@@ -2,9 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/ping',(req, res)=> {
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+app.use(express.json());
+
+// MongoDB connection URL
+const uri = process.env.mongoURi;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    // Connect to the MongoDB database
+    await client.connect();
+
+    // Check if the connection is successful
+    if (client.topology.isConnected()) {
+      res.json({ message: "pong", database_status: "Connected" });
+      console.log("yes");
+    } else {
+      res.json({ message: "pong", database_status: "Disconnected" });
+      console.log("no");
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// define the ping route
+app.get('/ping',(req,res)=>{
   res.send('pong');
-})
+});
+
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`🚀 server running on PORT: ${port}`);
